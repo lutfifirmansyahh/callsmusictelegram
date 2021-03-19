67e61ef72463841857b300959c9d563bbcb94d77
lutfifirmansyahh <80675535+lutfifirmansyahh@users.noreply.github.com>
2021-03-19 22:58:55 +0700

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -16,7 +16,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Ketik Ajah, Apa Yang Kamu Ingin Setel Lagu!...",
+            switch_pm_text="Ketik Aja, Apa Yang Ingin Kamu Dengar?...",
             switch_pm_parameter="help",
             cache_time=0
         )
